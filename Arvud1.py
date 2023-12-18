4eb622c1de119aa04acdfd76d34f089298f9024b
opilane <opilane@HP-CZC3217062>
2023-12-18 14:16:23 +0200

    Tunnitöö

---
+++ Arvud1.py
@@ -0,0 +1,8 @@
+algne_arv = int(input("Sisesta täisarv: "))
+järgmine_arv = algne_arv + 1
+tulemus = algne_arv + järgmine_arv
+tulemus = tulemus + 9
+tulemus = tulemus / 2
+tulemus = tulemus - algne_arv
+print(tulemus)
+ 
---
+++ parool.py
@@ -0,0 +1,8 @@
+sisestatud_pin = input("Sisesta PIN-kood: ")
+
+if sisestatud_pin == "1234":
+    print("sisenesid pangaautomaati")
+    
+else:
+    print("Vale parool! ligipääs keelatud")
+    
